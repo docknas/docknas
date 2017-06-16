42979abcd9d1bcbb81bc264399c0d14b6faaa171
Manuj Bhatia <manuj@pocketyourworld.com>
2017-06-15 20:00:51 -0700

    PKG/SETUP - Cleanup to reduce image size

--- pkg/setup/setup_fs.sh
+++ pkg/setup/setup_fs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -ex
 
+## TODO: testing only... figure out how to do proper permissions, so containers can read-write to
+## these directories
+umask 000
+
 mkdir -p /var/lib/docker && \
 mkdir -p /var/lib/sshd && \
 mkdir -p /var/lib/portainer && \
